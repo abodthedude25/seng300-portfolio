0af1b6eaf6ee4143ea8475b81c157eb2e30afe09
Abod Abbas <abdelrahman.abbas@ucalgary.ca>
2024-02-27 22:24:48 -0500

    added tech stacks and about me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,15 +20,15 @@ function About() {
           </p>
           <TypeAnimation
             sequence={[
-              "If I am not coding, you can find me: hiking",
+              "If I am not coding, you can find me: Reading",
               1000,
-              "If I am not coding, you can find me: surfing",
+              "If I am not coding, you can find me: Gyming",
               1000,
-              "If I am not coding, you can find me: traveling",
+              "If I am not coding, you can find me: Traveling",
               1000,
-              "If I am not coding, you can find me: filming",
+              "If I am not coding, you can find me: Benging Netflix",
               1000,
-              "If I am not coding, you can find me: jamming",
+              "If I am not coding, you can find me: Jamming",
               1000
             ]}
             wrapper="p"
@@ -42,53 +42,93 @@ function About() {
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'black' }}
-            contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="08/2023 – 05/2024"
             iconStyle={{ background: 'rgb(249, 38, 114)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Calgary, AB</h4>
-            <p>
-              Project Management, Business Development, Team Leading
-            </p>
+            <h3 className="vertical-timeline-element-title" style={{color: 'blue'}} >ML Systems Engineer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'blue'}}>Toronto, ON</h4>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'blue'}}>Qualcomm</h4>
+            <ul>
+              <li> <b>Compiled neural networks</b> to Qualcomm hardware, then analyzed results and prototyped algorithms to compilers and/or HW. <b>pybind11, TVM, Networking, Quantization.</b></li>
+              <li> Ran, debuged, and analyzed <b>performance simulations</b> and suggested enhancements to tackle bottlenecks due to computing and system memory hierarchy. <b>chromeTrace, Netron, Jit-Profiling.</b></li>
+              <li> Infastructed <b>Gitlab runners</b> to build and test servers to communicate with device clients, while caching to <b>reduce pipeline wait by 300%.</b></li>
+              <li> <b>Applied, discovered and researched</b> techniques to optimize existing LLM training and serving, as well as improving the model quality. <b>Transformers, Spill/Fill Analysis.</b></li>
+            </ul>
           </VerticalTimelineElement>
+
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="05/2023 – 09/2023"
             iconStyle={{ background: 'rgb(249, 38, 114)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Toronto, ON</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat amet numquam, quod, id, voluptas.
-            </p>
+            <h3 className="vertical-timeline-element-title" style={{color: 'orange'}} >Software Engineer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'orange'}}>Vancouver, BC</h4>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'orange'}}>Amazon</h4>
+            <ul>
+              <li> Wrote <b>ranking algorithm</b> for price, price-per-unit (PPU), reviews, and delivery-time. <b>Java</b>.</li>
+              <li> Fetch items from twister microservice and widget recommendations to <b>create a batch API request.</b></li>
+              <li> Updated the BuyAgain Detail page to accept new card features, which <b>updated the layout experience.</b></li>
+              <li> <b>Lazy loaded page details through ajaxing</b> from middleware microservice architecture.</li>
+              <li> <b>Integrated uiVerify tests</b> that validated the rendering of the skeleton card and the lazy loading of the Similar Items widget.</li>
+              <li> Setup general <b>OpEx CSM metrics (i.e. latency, availability traffic)</b> for the lazy loading and individual components for widgets.</li>
+            </ul>
           </VerticalTimelineElement>
+
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="04/2022 – 09/2022"
             iconStyle={{ background: 'rgb(249, 38, 114)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Berlin, Germany</h4>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat amet numquam, quod, id, voluptas.
-            </p>
+            <h3 className="vertical-timeline-element-title" style={{color: 'black'}} >Software Robotics Engineer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'black'}}>Berlin, Germany</h4>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'black'}}>DAI-Labor</h4>
+            <ul>
+              <li> Designed and implemented two full-stacks on a 1/10th sized car: <b>path-planning and autonomous-driving</b>.</li>
+              <li> Utilized robot middleware frameworks such as <b>ROS + Gazebo, Carla, and Blender in Ubuntu-Linux</b> to develop and integrate multi-sensor real-time systems with <b>navigation and computer vision algorithms.</b></li>
+              <li> Developed a Strong knowledge of data handling and integrating with multi-sensor real-time systems, such as <b>point-cloud, odometry, mapping, and SLAM.</b></li>
+              <li> Developed my knowledge of and experience in techniques for programming intelligent agent behaviors (e.g. <b>Behaviour Trees, Finite State Machines</b>), and Control Systems(e.g. <b>PID, Model Predictive Control</b>).</li>
+              <li> Demonstrated knowledge of Continuous Integration, Continuous Deployment, and Automated Testing Using <b>GitLab, Rviz2, Rqt, Docker, and Kubernetes.</b></li>
+            </ul>
           </VerticalTimelineElement>
+
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2020"
+            date="04/2021 - 09/2021"
             iconStyle={{ background: 'rgb(249, 38, 114)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat amet numquam, quod, id, voluptas.
-            </p>
+            <h3 className="vertical-timeline-element-title" style={{color: 'black'}} >Undergraduate Research Assistant</h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'black'}}>Calgary, AB</h4>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'black'}}>University of Calgary</h4>
+            <ul>
+              <li> Developed 73% of a COVID-19 Diagnoses Dashboard using <b>Flask and Python</b> to mainly develop <b>forms, databases, serial-port connections, RESTful API’s,</b> and middleware services.</li>
+              <li> Utilized advanced computer vision algorithms using <b>science kit, pytorch and tensorflow.</b></li>
+              <li> Experienced in developing web-based application front ends utilizing <b>HTML, CSS, and JavaScript.</b></li>
+              <li> Server-side development experience such as <b>NodeLinux, NoSQL databases, DevOps, cloud-based services and microservices,</b> unit and automated testing skills.</li>
+              <li> Actively participated in research projects under the guidance of Dr. Behrouz Far, the <b>graduate supervisor of the founder of Uber.</b></li>
+            </ul>
           </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="09/2021 - 04/2023"
+            iconStyle={{ background: 'rgb(249, 38, 114)', color: '#fff' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title" style={{color: 'black'}} >Software Engineer Club Member</h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'black'}}>Calgary, AB</h4>
+            <h4 className="vertical-timeline-element-subtitle" style={{color: 'black'}}>Schulich Racing</h4>
+            <ul>
+              <li> Spearheaded the design and implementation of a web-based dashboard that displays historic and <b>real-time telemetry data from a formula-style vehicle.</b></li>
+              <li> Developed custom API layer to handle all CRUD transactions and <b>JWT using Express.js</b></li>
+              <li> <b>Improved the efficiency and readability of 65%</b> of poorly maintained <b>React and Typescript</b> code modules through code refactoring, optimization, and documentation.</li>
+              <li> <b>Decreased memory latency by 45%</b> by implementing an in-memory data caching store using <b>Redis</b> to ingest, process, and analyze real-time data.</li>
+            </ul>
+          </VerticalTimelineElement>
+
         </VerticalTimeline>
       </div>
     </div>
--- src/components/section/Skill.js
+++ src/components/section/Skill.js
@@ -2,26 +2,43 @@ import React from 'react';
 import '../assets/style/Skill.scss';
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faJsSquare,
-  faHtml5,
-  faCss3Alt,
-  faSass,
-  faLess,
-  faNodeJs, 
-  faReact, 
-  faNpm, 
-  faJava, 
-  faUnity, 
+import { 
+  faReact,  
   faDocker, 
-  faGitAlt, 
-  faAws, 
-  faLinux, 
-  faAndroid, 
   faPython, 
-  faRProject, 
-  faMicrosoft 
+   
 } from '@fortawesome/free-brands-svg-icons';
+import DjangoPlain from 'devicons-react/lib/icons/DjangoPlain';
+import NpmOriginalWordmark from 'devicons-react/lib/icons/NpmOriginalWordmark';
+import ReactOriginal from 'devicons-react/lib/icons/ReactOriginal';
+import NodejsOriginalWordmark from 'devicons-react/lib/icons/NodejsOriginalWordmark';
+import Css3OriginalWordmark from 'devicons-react/lib/icons/Css3OriginalWordmark';
+import Html5OriginalWordmark from 'devicons-react/lib/icons/Html5OriginalWordmark';
+import JavascriptOriginal from 'devicons-react/lib/icons/JavascriptOriginal';
+import TypescriptOriginal from 'devicons-react/lib/icons/TypescriptOriginal';
+import FlaskOriginalWordmark from 'devicons-react/lib/icons/FlaskOriginalWordmark';
+import JavaOriginal from 'devicons-react/lib/icons/JavaOriginal';
+import MysqlOriginal from 'devicons-react/lib/icons/MysqlOriginal';
+import UnityOriginal from 'devicons-react/lib/icons/UnityOriginal';
+import GitOriginal from 'devicons-react/lib/icons/GitOriginal';
+import DockerOriginal from 'devicons-react/lib/icons/DockerOriginal';
+import AmazonwebservicesOriginalWordmark from 'devicons-react/lib/icons/AmazonwebservicesOriginalWordmark';
+import LinuxOriginal from 'devicons-react/lib/icons/LinuxOriginal';
+import AndroidstudioOriginal from 'devicons-react/lib/icons/AndroidstudioOriginal';
+import NextjsOriginal from 'devicons-react/lib/icons/NextjsOriginal';
+import PytorchOriginal from 'devicons-react/lib/icons/PytorchOriginal';
+import ApacheOriginal from 'devicons-react/lib/icons/ApacheOriginal';
+import MongodbOriginal from 'devicons-react/lib/icons/MongodbOriginal';
+import ScikitlearnOriginal from 'devicons-react/lib/icons/ScikitlearnOriginal';
+import MatlabOriginal from 'devicons-react/lib/icons/MatlabOriginal';
+import MatplotlibOriginal from 'devicons-react/lib/icons/MatplotlibOriginal';
+import NumpyOriginal from 'devicons-react/lib/icons/NumpyOriginal';
+import PythonOriginal from 'devicons-react/lib/icons/PythonOriginal';
+import COriginal from 'devicons-react/lib/icons/COriginal';
+import KubernetesOriginal from 'devicons-react/lib/icons/KubernetesOriginal';
+import LlvmOriginal from 'devicons-react/lib/icons/LlvmOriginal';
+import TrpcOriginal from 'devicons-react/lib/icons/TrpcOriginal';
+
 import Chip from '@material-ui/core/Chip';
 
 function Skill () {
@@ -34,17 +51,21 @@ function Skill () {
             <FontAwesomeIcon icon={faReact} size="4x"/>
             <h3>Full Stack Web Development</h3>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            I've built 10+ RESTful web apps, mainly with the MERN stack. Proficient in JavaScript libraries such as React, Next.js, Node, and Express. I also love using the Django and Flask.
             </p>
             <p className="flex-chips">Tech stack: 
-              <Chip variant="outlined" label="JavaScript" size="small" icon={<FontAwesomeIcon icon={faJsSquare} />} />
-              <Chip variant="outlined" label="HTML5" size="small" icon={<FontAwesomeIcon icon={faHtml5} />} />
-              <Chip variant="outlined" label="CSS3" size="small" icon={<FontAwesomeIcon icon={faCss3Alt} />} />
-              <Chip variant="outlined" label="SASS" size="small" icon={<FontAwesomeIcon icon={faSass} />} />
-              <Chip variant="outlined" label="LESS" size="small" icon={<FontAwesomeIcon icon={faLess} />} />
-              <Chip variant="outlined" label="NodeJS" size="small" icon={<FontAwesomeIcon icon={faNodeJs} />} />
-              <Chip variant="outlined" label="React" size="small" icon={<FontAwesomeIcon icon={faReact} />} />
-              <Chip variant="outlined" label="npm" size="small" icon={<FontAwesomeIcon icon={faNpm} />} />
+              <Chip variant="outlined" label="JavaScript" size="small" icon={<JavascriptOriginal  size="15" />} />
+              <Chip variant="outlined" label="TypeScript" size="small" icon={<TypescriptOriginal  size="15" />} />
+              <Chip variant="outlined" label="Flask" size="small" icon={<FlaskOriginalWordmark  size="25" />} />
+              <Chip variant="outlined" label="NEXT.js" size="small" icon={<NextjsOriginal size="18"/> } />
+              <Chip variant="outlined" label="HTML5" size="small" icon={<Html5OriginalWordmark  size="20" />} />
+              <Chip variant="outlined" label="CSS3" size="small" icon={<Css3OriginalWordmark  size="20" />} />              
+              <Chip variant="outlined" label="NodeJS" size="small" icon={<NodejsOriginalWordmark  size="30" />} />
+              <Chip variant="outlined" label="React" size="small" icon={<ReactOriginal  size="20" />} />
+              <Chip variant="outlined" label="npm" size="small" icon={<NpmOriginalWordmark  size="30" />} />
+              <Chip variant="outlined" label="Django" size="small" icon={<DjangoPlain size="12"/> } />
+              <Chip variant="outlined" label="MongoDB" size="small" icon={<MongodbOriginal size="20" />}/>
+              <Chip variant="outlined" label="TRPC" size="small" icon={<TrpcOriginal size="20" />}/>
             </p>
           </div>
 
@@ -52,36 +73,39 @@ function Skill () {
             <FontAwesomeIcon icon={faDocker} size="4x"/>
             <h3>Software Development</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            </p>
+            I enjoy working with agile teams to design and implement cross-platform solutions. Alongside my web development experience, I've created multiple video games using Unity. Also, I've worked with microcontrollers and sensors to create smart devices and IoT solutions.            </p>
             <p className="flex-chips">Tech stack: 
-              <Chip variant="outlined" label="Java" size="small" icon={<FontAwesomeIcon icon={faJava} />} />
-              <Chip variant="outlined" label="SQL" size="small" />
-              <Chip variant="outlined" label="C/C++/C#" size="small" />
-              <Chip variant="outlined" label="Unity" size="small" icon={<FontAwesomeIcon icon={faUnity} />} />
-              <Chip variant="outlined" label="Git" size="small" icon={<FontAwesomeIcon icon={faGitAlt} />} />
-              <Chip variant="outlined" label="Docker" size="small" icon={<FontAwesomeIcon icon={faDocker} />} />
-              <Chip variant="outlined" label="AWS" size="small" icon={<FontAwesomeIcon icon={faAws} />} />
-              <Chip variant="outlined" label="Linux" size="small" icon={<FontAwesomeIcon icon={faLinux} />} />
-              <Chip variant="outlined" label="Android Studio" size="small" icon={<FontAwesomeIcon icon={faAndroid} />} />
+              <Chip variant="outlined" label="Java" size="small" icon={<JavaOriginal size="20" />} />
+              <Chip variant="outlined" label="SQL" size="small" icon={<MysqlOriginal size="15" />}/>
+              <Chip variant="outlined" label="C/C++/C#" size="small" icon={<COriginal size="15" />}/>
+              <Chip variant="outlined" label="Unity" size="small" icon={<UnityOriginal size="20" />} />
+              <Chip variant="outlined" label="Git" size="small" icon={<GitOriginal size="20" />} />
+              <Chip variant="outlined" label="Docker" size="small" icon={<DockerOriginal size="20" />} />
+              <Chip variant="outlined" label="AWS" size="small" icon={<AmazonwebservicesOriginalWordmark size="20" />} />
+              <Chip variant="outlined" label="Linux" size="small" icon={<LinuxOriginal size="20" />} />
+              <Chip variant="outlined" label="Android Studio" size="small" icon={<AndroidstudioOriginal size="20" />} />
+              <Chip variant="outlined" label="Kubernetes" size="small" icon={<KubernetesOriginal size="20" />} />
             </p>
           </div>
 
           <div className="skill">
             <FontAwesomeIcon icon={faPython} size="4x"/>
-            <h3>Data Analysis</h3>
+            <h3>Machine Learning Systems</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            I've worked on multiple machine learning projects, from the model archeticture side all the way down to system level compilation. I'm proficient in Python and Pytorch, and I've used libraries such as Pandas, Matplotlib, and Sklearn.
             </p>
             <p className="flex-chips">Tech stack:
-              <Chip variant="outlined" label="Python" size="small" icon={<FontAwesomeIcon icon={faPython} />} />
-              <Chip variant="outlined" label="Pandas" size="small" icon={<FontAwesomeIcon icon={faPython} />} />
-              <Chip variant="outlined" label="Matplotlib" size="small" icon={<FontAwesomeIcon icon={faPython} />} />
-              <Chip variant="outlined" label="Sklearn" size="small" icon={<FontAwesomeIcon icon={faPython} />} />
-              <Chip variant="outlined" label="R" size="small" icon={<FontAwesomeIcon icon={faRProject} />} />
-              <Chip variant="outlined" label="Microsoft Excel" size="small" icon={<FontAwesomeIcon icon={faMicrosoft} />} />
-              <Chip variant="outlined" label="Snowflake" size="small" />
-              <Chip variant="outlined" label="Domo BI" size="small" />
+              <Chip variant="outlined" label="Python" size="small" icon={<PythonOriginal size="20"  />} />
+              <Chip variant="outlined" label="Numpy" size="small" icon={<NumpyOriginal size="20"  />} />
+              <Chip variant="outlined" label="Matplotlib" size="small" icon={<MatplotlibOriginal size="20"  />} />
+              <Chip variant="outlined" label="Matlab" size="small" icon={<MatlabOriginal size="20"  />} />
+              <Chip variant="outlined" label="Sklearn" size="small" icon={<ScikitlearnOriginal size="25"  />} />
+              <Chip variant="outlined" label="Pytorch" size="small" icon={<PytorchOriginal size="20" />} />
+              <Chip variant="outlined" label="C/C++" size="small" icon={<COriginal size="15" />}/>
+              <Chip variant="outlined" label="Apache TVM" size="small" icon={<ApacheOriginal size="20" />}/>
+              <Chip variant="outlined" label="LLVM" size="small" icon={<LlvmOriginal size="20" />}/>
+              <Chip variant="outlined" label="Onnx" size="small" />
+
             </p>
           </div>
         </div>
